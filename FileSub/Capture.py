87c216877e62923cea8eaed3d6c3e6f37d21fcf5
David Gonzalez <dgonzalez33@miners.utep.edu>
2017-04-26 23:57:03 +0000

    filesystem up and running

--- FileSub/Capture.py
+++ FileSub/Capture.py
@@ -1,9 +1,18 @@
 from Tshark_Handler import Tshark_Handler
 class Capture:
-    def isPDML(filePath):
-        filePath.lower().endswith('.pdml')
+    def createCapture(self, filePath):
+        if(self.isCapture(filePath)):
+            self.filePath = filePath
+            if(self.isPDML(filePath) is False):
+                self.filePath = Tshark_Handler().createPDML(filePath)
+        else:
+            self.filePath = None
+        return self.filePath
     def __init__(self, filePath):
-        self.filePath = filePath
-        if(isPDML(filePath) is False):
-            self.filePath = createPDML(filePath)
-    
+        return
+    def isPDML(self,filePath):
+        return filePath.lower().endswith('.pdml')
+    def isCapture(self, filePath):
+        return filePath.lower().endswith(('.pdml','.pcap','pcapng'))
+    def getFilePath(self):
+        return self.filePath
