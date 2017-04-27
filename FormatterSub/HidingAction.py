1a9d135609f2935d6aed1ad625f98bc84ad66f2c
Ezequiel Rios <rioszeke@localhost.localdomain>
2017-04-27 00:26:19 -0600

    adding improved hide action. Left note inside, implementation may need to be rethought

--- FormatterSub/HidingAction.py
+++ FormatterSub/HidingAction.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 
-from Action import Action
+from FormatterSub.Action import Action
+from PDMLSub.FieldElement import fieldelement
+from FormatterSub.RenamingAction import RenamingAction
 
+### This class is a bit useless since it does what renaimg does anyway
+### For future note we may have to get rid of this class altogether if
+### We cannont find another reason to have it
 class HidingAction(Action):
 
     def getActionResult(self, target, attribute, value):
-        index = target.find(attribute)
-        if index == -1:
-            index = target.find(">")
-            newString = target[:index] + " Hidden=\""+value+"\""+target[index:]
-        else:
-            index = target.find("\"",index,len(target))
-            index2 = target.find("\"",index+1,len(target))
-            newString = target[:index+1] + value + target[index2:]
-        return newString
+        renameObject = RenamingAction()
+        return renameObject.getActionResult(target, attribute, value)
