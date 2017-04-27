0f0abd0ca19fdfa5a962b8b9d86d0553962bb1f2
Ezequiel Rios <rioszeke@localhost.localdomain>
2017-04-27 14:36:04 -0600

    commiting  minor changes to annotating action

--- FormatterSub/AnnotatingAction.py
+++ FormatterSub/AnnotatingAction.py
@@ -2,7 +2,7 @@
 
 from FormatterSub.Action import Action
 from PDMLSub.FieldElement import fieldelement
-
+from PDMLSub.RenamingAction import RenamingAction
 class AnnotatingAction(Action):
 
     def getActionResult(self, target, attribute, value):
