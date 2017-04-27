e5e56a0cc3b4a9f76847f636767bb158adee27fa
ogordillo <ogordillo@miners.utep.edu>
2017-04-27 00:53:28 -0600

    Add files via upload

--- RestofSystem/Controller.py
+++ RestofSystem/Controller.py
@@ -1,33 +1,36 @@
 
 from PDMLSub.PDMLManager import pdmlmanager
 from windows.WindowController import WindowController
-
+from gi.repository import GObject
 
 class controller:
-    
-    packets = []
-    pdml_attributes = {}
-    protos = []
-    
-    p_manager = pdmlmanager()
-    w_controller = WindowController()
-    w_controller.main()
+
+    p_manager = pdmlmanager("../Scripts/dns_query_response2.pdml")
+    w = WindowController()
+
     
     def __init__(self):
         self.create_self()
-        self.packets = []
-        self.protos = []
-        self.pdml_attributes = {}
+        GObject.idle_add(self.update_after)
+        self.w.main()
+
      
     def create_self(self):
-        #print("pdml class instantiated")
+        #print("hello")
+        #print(self.p_manager.get_pdml_as_text())
         return 0
-        
-        
     
+    def update_after(self):
+        print("after")
+        #self.w.packet_widget.set_packet_window_text(self.p_manager.get_pdml_as_text())
+        return 0;
+        
     
 
 
 if(__name__ == "__main__"):
+    GObject.threads_init()
     d = controller()
+    
+
     
