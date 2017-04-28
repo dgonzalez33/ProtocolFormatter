c5a2da29623ded13c3b5e7b27642b5c1ea9d7a7c
Ezequiel Rios <rioszeke@localhost.localdomain>
2017-04-27 18:35:01 -0600

    adding superclass action changes

--- FormatterSub/Action.py
+++ FormatterSub/Action.py
@@ -2,5 +2,26 @@
 from PDMLSub.FieldElement import fieldelement
 
 class Action:
-    def getActionResult(target, attribute, value):
-        print("doing nothing")
+    def __init__ (self, attribute, value):
+        self.attribute = attribute
+        self.value = value
+
+    def setAttrib(self, attribute):
+        self.attribute = attribute
+
+    def getAttrib(self):
+        return self.attribute
+
+    def setValue(self, value):
+        self.value = value
+
+    def getValue(self):
+        return self.value
+
+    def getActionResult(self, target):
+        newFieldElem = fieldelement()
+        oldAttrib = target.get_field_attributes()
+        for key in oldAttrib:
+            newFieldElem.set_field_attrib(key, oldAttrib[key])
+        newFieldElem.set_field_attrib(self.attribute, self.value)
+        return newFieldElem
