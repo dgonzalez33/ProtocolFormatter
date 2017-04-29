f12f0bc74eb363c25c1600154c5edd8a37e5197d
Ezequiel Rios <rioszeke@localhost.localdomain>
2017-04-29 14:11:21 -0600

    commiting changes to RenamingAction class

--- FormatterSub/RenamingAction.py
+++ FormatterSub/RenamingAction.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from FormatterSub.Action import Action
-from PDMLSub.FieldElement import fieldelement
+# from PDMLSub.FieldElement import fieldelement
 class RenamingAction(Action):
 
     def getActionResult(self, target):
