5873fe9446b13ee73dbea45715fda5e8fc66c94c
ogordillo <ogordillo@miners.utep.edu>
2017-04-29 02:54:30 -0600

    integrated Capture into Window Controller

--- FileSub/Tshark_Handler.py
+++ FileSub/Tshark_Handler.py
@@ -5,7 +5,7 @@ class Tshark_Handler:
     def createPDML(self, filePath):
         basename = (os.path.splitext(filePath)[0])
         with open(basename+".pdml", 'w') as f:
-            call(["tshark","-r","rand.pcapng","-T","pdml"], stdout=f)
+            call(["tshark","-r",filePath,"-T","pdml"], stdout=f)
         return basename+".pdml"
     def __init__(self):
         return
