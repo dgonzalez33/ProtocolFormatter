29c80b3fde636c63c6b616eace77131d4b1a07f6
djarmas <djarmas@users.noreply.github.com>
2017-05-06 16:58:18 -0600

    updated FilterTest

--- Tests/FilterTest.py
+++ Tests/FilterTest.py
@@ -1,12 +1,28 @@
 import unittest
-from Filter import Filter
+import sys
+
+sys.path.insert(0, '../')
+from FormatterSub.Filter import Filter
 
 class FilterTest(unittest.TestCase):
 
 	def setUp(self):
-		self.newelement1 = Filter()
-		self.newelement2 = Filter()
-		self.netelement3 = Filter()
-		self.newelement1.saveFilter("testFilter")
-		self.newelement2.saveFilter("")
-		self.newelement3.saveFilter(3)
+		self.newfilter1 = Filter()
+		self.newfilter2 = Filter()
+		self.newfilter3 = Filter()
+		
+		
+	def test_saveFilter(self):
+		self.newfilter1.saveFilter("testFilter")
+		self.newfilter2.saveFilter("")
+		self.newfilter3.saveFilter("3")
+		
+		self.assertEqual(self.newfilter1.fileExists("testFilter"),1,"Filter Exists")
+		
+		self.assertEqual(self.newfilter2.fileExists(""),1,"Filter Exists")
+		
+		self.assertEqual(self.newfilter3.fileExists("3"),0,"Filter does not exist")
+		
+		
+if __name__ == '__main__':
+	unittest.main()
