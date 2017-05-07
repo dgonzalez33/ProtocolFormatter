5d658b11f1ed7866cf31dfff80d2fd18c71c7772
djarmas <djarmas@users.noreply.github.com>
2017-05-06 19:30:14 -0600

    updated rule test

--- Tests/RuleTest.py
+++ Tests/RuleTest.py
@@ -2,8 +2,17 @@ import unittest
 import os
 
 from FormatterSub.Rule import Rule
+from PDMLSub.PDMLManager import pdmlmanager
 
 class RuleTest(unittest.TestCase):
 
 	def setUp(self):
-		self.newManager = 
+		self.testManager = pdmlmanager()
+		self.testRule = Rule()
+		
+	def testapplyRules(self):
+		self.testRule.applyRules(testManager)
+		
+		
+if __name__ == '__main__':
+	unittest.main()
