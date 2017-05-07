d136c66dd2b76e1fb2f3852ea012c73bf5d4d3f3
djarmas <djarmas@users.noreply.github.com>
2017-05-06 19:59:43 -0600

    updated rule test

--- Tests/RuleTest.py
+++ Tests/RuleTest.py
@@ -7,7 +7,7 @@ from PDMLSub.PDMLManager import pdmlmanager
 class RuleTest(unittest.TestCase):
 
 	def setUp(self):
-		self.testManager = pdmlmanager()
+		self.testManager = pdmlmanager("Scripts/dns_query_response2.pdml")
 		self.testRule = Rule()
 		
 	def testapplyRules(self):
